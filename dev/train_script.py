95eef8d2fdf43966f40b1d265001aa0241169e4c
Leah Zuckerman <LeahDZuckerman@gmail.com>
2024-03-19 10:41:33 -0700

    cleaning

--- dev/train_script.py
+++ dev/train_script.py
@@ -1,8 +1,3 @@
-#########################################
-## Current GPU train loop              ##
-## Own libraries loaded in the bottom  ##
-#########################################
-
 import torch, pickle, time, os
 import numpy as np
 import os.path as osp
