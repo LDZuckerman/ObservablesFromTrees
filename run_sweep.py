1354bcc0603f315604b3253f785c3140906da948
Leah Zuckerman <LeahDZuckerman@gmail.com>
2024-05-31 07:30:23 -0700

    fix experiment sweep

--- run_sweep.py
+++ run_sweep.py
@@ -1,95 +1,97 @@
 '''
-NOTE: For any keys swept over (e.g. in diff file), any values set for those keys in the base file are overwritten in each run.
-      However, the base file will not be modified.
-      This is to allow for an existing exp file to be used as the base file. 
+Perform a hyperparameter search by running a large number of experiments.
+
+Each experiment:
+    Saves trained model to sweep directory.
+    Adds dict of final validation results to results file (not test results - compute those separately for best models)
+    Adds dict of experiment parameters to done experiments file.
+
+Input files: 
+    Difffile = dict containing lists of valyes to sweep through for parameters to explore.
+    Basefile = experiment dict containing values to set for parameters held constant (not that base file itself is modified, so can be existing exp file)
+
+NOTE: If another sweep is run on the same task, experiments will be added to the same output directory. 
+      If multiple seperate sweeps are desired (e.g. two different datasets), could add sweep id to base file and modify below to create a new output directory for each sweep.
 '''
 
-import os, json, shutil, argparse
+import os, shutil, argparse
+import json, pickle
 import os.path as osp
 import pandas as pd
 import numpy as np
 import dev.train_script as train_script
 import dev.run_utils as run_utils
+import multiprocessing as mp
+import copy
 
 # Read in arguements 
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--f", type=str, required=True)
 parser.add_argument("-d", "--d", type=str, required=True)
 parser.add_argument("-gpu", "--gpu", type=str, required=False, default='1')
-parser.add_argument("-redo", "--redo", type=str, required=False, default='False')
 parser.add_argument("-resume", "--resume", type=str, required=False, default='False')
+parser.add_argument("-add", "--add", type=str, required=False, default='False')
 args = parser.parse_args()
 
-# Load base dictionary 
-basefile = osp.expanduser(args.f) # osp.join(osp.abspath(''), args.f)
-basedict = json.load(open(basefile))
+# Load base and diff dicts
+basedict = json.load(open(osp.expanduser(args.f)))
+diff = json.load(open(osp.expanduser(args.d)))
+sweep_dict = diff['sweep_params']
+sweep_id = diff['sweep_id']
 
-# Get param sets list from diff dictionary
-difffile = osp.expanduser(args.d)
-diffdict = json.load(open(difffile))
-diffkeys = list(diffdict.keys())
-sets = run_utils.perms(diffdict) 
-print(f"RUNNING SWEEP OF {len(sets)} EXPERIMENTS\n")
-print(f"Exploring the following paramsters: {diffkeys}")
-sets = pd.DataFrame(columns = list(diffdict.keys()), data = sets)
-
-# Set output directory: one for entire sweep, not for each exp
-try: 
-    swp_id = basedict['swp_id'] # if sweep id is specified in basefile, use that
-    if 'sweep' not in swp_id: raise ValueError(f"WARNING: sweep id should contain 'sweep'.")
-except:
-    swp_id =f"sweep{basedict['exp_id'][3:]}" # if using previous exp file as basefile (e.g. if using exp9/expfile.json, swp_id = sweep9)
-out_pointer = osp.expanduser(f"{basedict['task_dir']}{swp_id}") 
-if eval(args.redo):
-    print(f"Redo flag is True, deleting output directory {out_pointer}/")
-    shutil.rmtree(out_pointer)
-if not osp.exists(out_pointer):
-    print(f"Makeing output directory {out_pointer}/")
-    os.makedirs(out_pointer)
-elif osp.exists(out_pointer) and not eval(args.resume) and not eval(args.redo):
-    raise ValueError(f"   CUATION: Sweep output directory {out_pointer}/ already has some models in it, and both redo and resume flags are False.")
-
-# Copy base file and diff file to output directory
-shutil.copy(basefile, osp.join(out_pointer, 'basefile.json'))
-shutil.copy(difffile, osp.join(out_pointer, 'difffile.json'))
-
-# Create file to save done experiments
-donefile = osp.join(out_pointer, 'done_experiments.json')
-print(f"Creating {donefile} to save done experiments")
-json.dump([], open(donefile, 'w'))
-
-# If resume, find the last experiment done and start from the next one
-if eval(args.resume):
-    sets = run_utils.find_next_exp(out_pointer, donefile, sets)
-
-# Loop over experiments prescribed by diff file
-count = 0
-for i in range(len(sets)):
-
-    # Create new dict and set unique exp id
-    dict = basedict.copy()
-    expnum = run_utils.get_swp_num(out_pointer) # This should also allow for resuming
-    dict['exp_id'] = f"{swp_id}_{expnum}"
-    print(f"\nSETTING UP EXPERIMENT {dict['exp_id']}\n') # print(f'\nParameters are the following -> \n{dict}")
-
-    # Modify dict to use params in set, and to ensure low output
-    set = sets.iloc[i]
-    print(f"Modified keys for this run: {set.to_dict()}")
-    for groupkey in ['data_params', 'learn_params', 'hyper_params', 'run_params']:
+# Get list of config dicts using base and diff files
+diffsets = run_utils.perms(sweep_dict) 
+diffsets = pd.DataFrame(data=diffsets, columns=sweep_dict.keys())
+all_paramdicts = []
+for i in range(len(diffsets)):
+    dict = copy.deepcopy(basedict)
+    diffset = diffsets.iloc[i]
+    for groupkey in ['learn_params', 'hyper_params', 'run_params']: # assume keeping data params constant (perhaps do another sweep if want to change them)
         for key in basedict[groupkey].keys():
-            if key in diffkeys:
-                dict[groupkey][key] = set[key]
-    dict['run_params']['performance_plot'] = None
-    dict['run_params']['save_plot'] = False 
-    dict['run_params']['log'] = False 
-    dict['run_params']['n_trials'] = 1
+            if key in sweep_dict.keys():
+                dict[groupkey][key] = diffset[key]
+    expid = f"exp{sweep_id.replace('sweep','')}{'{:03d}'.format(i)}" # e.g. exp0001 for first experiment in sweep0
+    dict['exp_id'] = expid
+    all_paramdicts.append(dict)
+if len(all_paramdicts) > 999: raise ValueError(f"Too many experiments in sweep. Need to add more digits to exp_id to accomodate.")
+print(f"RUNNING SWEEP OF {len(all_paramdicts)} EXPERIMENTS\n", flush=True)
 
-    # Train (train and val)
-    train_script.run(dict['run_params'], dict['data_params'], dict['learn_params'], dict['hyper_params'], out_pointer, dict['exp_id'], args.gpu, low_output=True)
+# Create sweep output directory and save the base file, diff dile, and list of param dicts
+sweep_dir = osp.expanduser(f"{basedict['task_dir']}/sweep") 
+os.makedirs(sweep_dir)
+shutil.copy(osp.expanduser(args.f), sweep_dir)
+shutil.copy(osp.expanduser(args.d), sweep_dir)
+pickle.dump(all_paramdicts, open(f"{sweep_dir}/paramdicts.pkl", "wb"))
 
-    # Save experiment to done file
-    dicts = json.load(open(donefile, 'r'))
-    dicts.append(dict)
-    json.dump(dicts, open(donefile, 'w')) # open file twice to ensure overwrite
-    count += 1
-    print(f'DONE with {expid} [{count}/{len(sets)}]\n')
+# # Create temporary output directory for threads to deposit configs, trained modls, and results files using temporary IDs 
+# procs_outdir = f"{out_pointer}/procs_temp" 
+# if osp.exists(procs_outdir): 
+#     raise ValueError(f"Temporary output directory {procs_outdir} already exists. Should delete before adding more experiments to sweep.")
+# print(f"Creating temporary output directory {procs_outdir} for each thread to store tree and meta files.")
+# os.makedirs(procs_outdir)   
+
+# Set up multithreading processes            
+n_threads = 10  
+paramdict_sets = np.array_split(all_paramdicts, n_threads)
+print(f"Splitting the {len(all_paramdicts)} param sets into {len(paramdict_sets)} sets of ~{len(paramdict_sets[0])} files each, to be processed by {n_threads} threads.")
+
+# Run each experiment by passing the sets of parmater sets to the threads
+pool = mp.Pool(processes=n_threads) 
+results = []
+for i in range(len(paramdict_sets)):
+    print(f"Starting thread {i} with {len(paramdict_sets[i])} experiments.")
+    paramdicts = paramdict_sets[i]
+    args = (paramdicts, sweep_dir, i)
+    result = pool.apply_async(run_utils.run_sweep_proc, args=args)#), callback=response)
+    results.append(result.get())
+pool.close()
+pool.join()
+if len(results) != 0:
+    for i in range(len(results)):
+        print(f"proc {i}: {results[i]}")
+    raise ValueError('Some threads failed. Check above for error messages.')
+
+# # Move the configs, trained models, and results files from the temporary threads directory into the sweep folder with permanent unique IDs
+# print(f"\nDONE ALL THREADS. Moving outputs from temporary dir to sweep dir.\n")
+# run_utils.move_thread_outputs(procs_outdir, out_pointer)
+print(f"DONE")
--- run_sweep.sh
+++ run_sweep.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -l
 
-#SBATCH -J E%j                    # aka "job-name" (%j gives job id)
+#SBATCH -J S%j                    # aka "job-name" (%j gives job id)
 #SBATCH -p gpu                    # aka "partition"
 #SBATCH -t 32:59:00               # aka "time": maximum time for run hh:mm:ss
 #SBATCH -C v100                   # aka "constraint": use nodes with only listed feature 
 #SBATCH -N 1                      # aka "nodes": number of nodes to request
-#SBATCH --gpus=4                  # total GPUs to request 
+#SBATCH --gpus=1                  # total GPUs to request 
 #SBATCH --mem=160G                # total memory to request     
-#SBATCH --output=SlurmOut/E%j.out # path relative to WORKING directory, NOT directory of this file
+#SBATCH --output=SlurmOut/S%j.out # path relative to WORKING directory, NOT directory of this file
 
 
 module purge
@@ -16,17 +16,16 @@ module load gcc
 module load cuda
 source ~/VENVS/torch_env2/bin/activate
 
-redo='False'
-resume='False'
 
-while getopts "f:d:r:s" flag; do
+while getopts "f:d:" flag; do
  case $flag in
    f) expfile=$OPTARG;;
    d) difffile=$OPTARG;;
-   r) redo=$OPTARG;;
-   s) resume=$OPTARG;;
  esac
 done
 
-srun python ObservablesFromTrees/run_sweep.py -f $expfile -d $difffile -gpu 1 -redo $redo -resume $resume 
+srun python ObservablesFromTrees/run_sweep.py -f $expfile -d $difffile -gpu 1
 
+####
+# sbatch ObservablesFromTrees/run_sweep.sh -f Task_phot/expfile.json -d Task_phot/difffile.json
+####
